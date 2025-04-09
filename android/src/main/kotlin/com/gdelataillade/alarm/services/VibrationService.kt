e285ecfe4e7fa97cc27badef1cfcb2d93340b89b
pungpondstr <50855616+pungpondstr@users.noreply.github.com>
2025-04-09 16:54:43 +0700

    Update VibrationService.kt

--- android/src/main/kotlin/com/gdelataillade/alarm/services/VibrationService.kt
+++ android/src/main/kotlin/com/gdelataillade/alarm/services/VibrationService.kt
@@ -4,22 +4,31 @@ import android.content.Context
 import android.os.Build
 import android.os.VibrationEffect
 import android.os.Vibrator
+import android.os.VibratorManager
 
 class VibrationService(context: Context) {
-    private val vibrator: Vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+//    private val vibrator: Vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+    private val vibrator: Vibrator? = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        val vibratorManager = context.getSystemService(VibratorManager::class.java)
+        vibratorManager?.defaultVibrator
+    } else {
+        @Suppress("DEPRECATION")
+        context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+    }
 
     fun startVibrating(pattern: LongArray, repeat: Int) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             // Use VibrationEffect for API 26 and above
             val vibrationEffect = VibrationEffect.createWaveform(pattern, repeat)
-            vibrator.vibrate(vibrationEffect)
+            vibrator?.vibrate(vibrationEffect)
         } else {
             // Use the older vibrate method for below API 26
-            vibrator.vibrate(pattern, repeat)
+            @Suppress("DEPRECATION")
+            vibrator?.vibrate(pattern, repeat)
         }
     }
 
     fun stopVibrating() {
-        vibrator.cancel()
+        vibrator?.cancel()
     }
 }
